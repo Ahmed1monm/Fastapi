20212ce1a11ef3b08ad8dee524be3198bec143e3
Ahmed Moniem <ahmedmoniem922@gmail.com>
2022-09-03 16:39:16 +0200

    start of http exceptions and status code

--- fastAPI/book2.py
+++ fastAPI/book2.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
 from uuid import UUID
 
@@ -10,7 +10,7 @@ app = FastAPI()
 class Book(BaseModel):
     title: str = Field(min_length=5)
     description: Optional[str] = Field(min_length=5, max_length=150)
-    auther: str = Field(min_length=2, max_length=100)
+    author: str = Field(min_length=2, max_length=100)
     rating: int = Field(gt=-1, lt=101)
     id: UUID
 
@@ -38,3 +38,62 @@ async def add_book(book: Book):
         "your data": book,
         'data': Books
     }
+
+
+@app.get("/")
+async def read_all_books(books_to_return: Optional[int] = None):
+    # if books_to_return and books_to_return < 0:
+    #     raise NegativeNumberException(books_to_return=books_to_return)
+
+    if len(Books) < 1:
+        create_books_no_api()
+
+    if books_to_return and len(Books) >= books_to_return > 0:
+        i = 1
+        new_books = []
+        while i <= books_to_return:
+            new_books.append(Books[i - 1])
+            i += 1
+        return new_books
+    return Books
+
+
+def create_books_no_api():
+    book_1 = Book(id="71f4c2ea-1340-41f4-89f7-2852347bb0d1",
+                  title="Title 1",
+                  author="Author 1",
+                  description="Description 1",
+                  rating=60)
+    book_2 = Book(id="21f4c2ea-1340-41f4-89f7-2852347bb0d1",
+                  title="Title 2",
+                  author="Author 2",
+                  description="Description 2",
+                  rating=70)
+    book_3 = Book(id="31f4c2ea-1340-41f4-89f7-2852347bb0d1",
+                  title="Title 3",
+                  author="Author 3",
+                  description="Description 3",
+                  rating=80)
+    book_4 = Book(id="41f4c2ea-1340-41f4-89f7-2852347bb0d1",
+                  title="Title 4",
+                  author="Author 4",
+                  description="Description 4",
+                  rating=90)
+    Books.append(book_1)
+    Books.append(book_2)
+    Books.append(book_3)
+    Books.append(book_4)
+
+
+@app.delete('/')
+async def delete_book(book_id: UUID):
+    count = 0
+    for book in Books:
+        count += 1
+        if book_id == book.id:
+            del Books[count - 1]
+            return {
+                'Msg': f'Book with id = {book_id} deleted successfully'
+            }
+
+    raise HTTPException(status_code=400, detail=f'There is No books with id = {book_id}')
