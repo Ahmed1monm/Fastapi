b4dac2d8c3c60b3be6dacfadd171a059f31b935e
Ahmed Moniem <ahmedmoniem922@gmail.com>
2022-09-06 17:17:40 +0200

    User model and relationships

--- fastAPI/TodoApp/models.py
+++ fastAPI/TodoApp/models.py
@@ -1,6 +1,19 @@
-
-from sqlalchemy import Column, Boolean, Integer, String
+from sqlalchemy import Column, Boolean, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from database import Base
+
+
+class Users(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    username = Column(String, unique=True, index=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    todos = relationship('Todo',  # Class name not table name
+                         back_populates='owner')
 
 
 class Todo(Base):
@@ -11,3 +24,5 @@ class Todo(Base):
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey('users.id'))
+    owner = relationship('Users', back_populates='todos')
