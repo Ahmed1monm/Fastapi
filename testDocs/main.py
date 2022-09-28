c99c0fffdf0c0a7b4402b85dd119bbc5afd1228a
Ahmed Moniem <ahmedmoniem922@gmail.com>
2022-09-28 12:12:00 +0200

    Files

--- testDocs/main.py
+++ testDocs/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query, Path
+from fastapi import FastAPI, Query, Path, File, UploadFile
 from typing import List, Union
 from pydantic import BaseModel
 
@@ -41,6 +41,65 @@ class Item(BaseModel):
 
 # Notice that response_model is a parameter of the "decorator" 
 # method (get, post, etc). Not of your path operation function, like all the parameters and body.
-@app.post("/items/", response_model=Item)
+
+#*----------------------------------------------------------------------
+#*FastAPI will use this response_model to:
+# Convert the output data to its type declaration.
+# Validate the data.
+# Add a JSON Schema for the response, in the OpenAPI path operation.
+# Will be used by the automatic documentation systems.
+# But most importantly:
+# Will limit the output data to that of the model
+
+@app.post("/items/", response_model=Item)  
 async def create_item(item: Item):
     return item
+
+#---------------------------------------------------------------------
+class UserIn(BaseModel):
+    username: str
+    password: str
+    email: str
+    full_name: Union[str, None] = None
+
+
+class UserOut(BaseModel):
+    username: str
+    email: str
+    full_name: Union[str, None] = None
+
+
+@app.post("/user/", response_model=UserOut) #* Will cast UserIn to be UserOut 
+async def create_user(user: UserIn):
+    return user
+
+
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+}
+
+
+# sometimes you have default values for some fields in your model
+# you not want to send default and optional fields that have null values 
+# make response_model_exclude_unset=True 
+@app.get("/items/{item_id}", response_model=Item, response_model_exclude_unset=True)
+async def read_item(item_id: str):
+    return items[item_id]
+
+# @app.post("/files/")
+# async def create_file(file:Union [bytes , None] = File(default=None)):
+#     if not file:
+#         return {"message": "No file sent"}
+#     else:
+#         return {"file_size": len(file)}
+
+
+# @app.post("/uploadfile/")
+# async def create_upload_file( file:Union[ UploadFile , None]  = None ):
+#     if not file:
+#         return {"message": "No upload file sent"}
+#     else:
+#         return {"filename": file.filename}
+
